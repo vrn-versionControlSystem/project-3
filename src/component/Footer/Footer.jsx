27c9fa6e0e06fd1cbf4a91a8baa04f2c71667000
vasim <vrnadaf@gmail.com>
2025-06-11 19:06:37 +0530

    upper footer work is done

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -6,25 +6,8 @@ const Footer = () => {
         <div className='container-fluid footer-container-fluid'>
 
             <div className='container home-container'>
-                <div className='row footer-row1'>
-                    <div className='col-lg-12 footer-col3'>
-
-
-                        <ul>
-                            <li><h5>MANDATORY DISCLOSURE</h5>  </li>
-                            <li> Policies of Bank</li>
-                            <li> DICGC Details</li>
-                            <li> DEAF Scheme</li>
-                            <li>DEAF Account List</li>
-                            <li>MOUs details</li>
-                            <li>Annual Reports</li>
-                            <li>Financial Statement</li>
-                            <li> Right to Information</li>
-                            <li> Discontinuation of the padho pardesh interest subsidy scheme</li>
-                            <li>ACSIS - Dr. Ambedkar Central Scheme of Interest Subsidy on educational loan for OBCs and EBCs during FY 2023-24</li>
-                        </ul>
-
-
+                <div className='row footer-row2'>
+                    <div className='col-lg footer-col3'>
                         <ul>
                             <li><h5> RATES AND CHARGES</h5></li>
                             <li> Deposit Interest Rates</li>
@@ -38,25 +21,50 @@ const Footer = () => {
                             <li>SARFAESI Notice / Sale Notice</li>
 
                         </ul>
+                    </div>
+                    <div className='col-lg footer-col3'>
+                        <ul>
+                            <li><h5>MANDATORY DISCLOSURE</h5>  </li>
+                            <li> Policies of Bank</li>
+                            <li> DICGC Details</li>
+                            <li> DEAF Scheme</li>
+                            <li>DEAF Account List</li>
+                            <li>MOUs details</li>
+                            <li>Annual Reports</li>
+                            <li>Financial Statement</li>
+                            <li> Right to Information</li>
+                            <li> Discontinuation of the padho pardesh interest subsidy scheme</li>
+                            <li>ACSIS - Dr. Ambedkar Central Scheme of <br />
+                                Interest Subsidy on educational loan for
+                                <br /> OBCs and EBCs during FY 2023-24</li>
+                        </ul>
+                    </div>
+                    <div className='col-lg footer-col3'>
 
                         <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li><h5>STAFF RELATED</h5></li>
+                            <li>Staff Details along with Remuneration </li>
+                            <li>Service Regulation </li>
+                            <li>Careers and Recruitments</li>
+                            <li>Pension Portal</li>
+                            <li>Pension Regulation</li>
+                            <li>CLWS Guidelines</li>
+                            <li>Reservation of Roster </li>
+                            <li>Format to be Submitted by EX-EMPLOYEES</li>
+                            <li>Format to be Submitted by Family Members of Deceased Employees </li>
+                            <li>Computer Increment Arrears from 01/11/1993</li>
                         </ul>
 
 
 
 
                     </div>
+
+
+
+                </div>
+
+                <div className='row footer-row1'>
 
 
                     <div className='col-lg-12 footer-col1'>
